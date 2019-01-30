a513a273d3c5810babd3a1b3521b1b290ec1c246
lidongyuan <1879087903@qq.com>
2019-01-30 14:44:35 +0800

    redtyer

--- src/launch/Test.java
+++ src/launch/Test.java
@@ -15,7 +15,7 @@ public class Test {
 	public static void main(String[] args) {
     LFrame_Menu frame = new LFrame_Menu("开始游戏");
     frame.addTheme("真五国无双之潜龙入梦");
-    frame.addBackground("c:/timg.jpg");
+    frame.addBackground("/resourse/timg.jpg");
     frame.addOption("开始游戏");
     frame.addOption("载入存档");
     frame.addOption("成就一览");
